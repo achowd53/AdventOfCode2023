b8188f32cf692f8c801ea1079a1c56ec5f138a7b
aribchowdhury <aribchowdhury03@gmail.com>
2023-12-02 00:31:45 -0500

    Day 2, got stuck on reading comprehension 1607/1200

--- day2/day2.py
+++ day2/day2.py
@@ -15,34 +15,25 @@ from z3 import Solver, Int, And
 #Python has complex numbers of the form x+yj (1+1j) or complex(x,y) (complex(1,1))
 
 def runPart1(filename):    
-    #lines = open(filename)
-    #lines = open(filename).read().split('\n\n')
     lines = open(filename).read().splitlines()
-    #lines = list(map(int,lines))
-        
-    ans = None
+    ans = 0
     for line in lines:
-        #a,b = line.split()
-        pass
+        id = int(re.findall(r'Game (\d+):', line)[0])
+        if all([int(match)<=limit for color,limit in[("red",12),("green", 13),("blue",14)]for match in re.findall(rf'(\d+) {color}',line)]):
+            ans += id
     return ans
 
 def runPart2(filename):    
-    #lines = open(filename)
-    #lines = open(filename).read().split('\n\n')
     lines = open(filename).read().splitlines()
-    #lines = list(map(int,lines))
-        
-    ans = None
+    ans = 0
     for line in lines:
-        #a,b = line.split()
-        pass
+        ans += prod(max(int(match)for match in re.findall(rf'(\d+) {color}',line))for color in["red", "green", "blue"])
     return ans
         
 
 print("Running example.txt...")
 print("Part 1:", runPart1("example.txt"))
 print("Part 2:", runPart2("example.txt"))
-#print("Part 2:", runPart2("example2.txt"))
 print()
 print("Running input.txt...")
 print("Part 1:", runPart1("input.txt"))
