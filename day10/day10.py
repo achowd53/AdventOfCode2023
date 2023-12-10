47234482109766fe77b17f6f4a9f91c1a47932a5
aribchowdhury <aribchowdhury03@gmail.com>
2023-12-10 01:37:41 -0500

    Day10, 788/176, Added Shapely to template for geometry problems

--- day10/day10.py
+++ day10/day10.py
@@ -8,7 +8,7 @@ from string import ascii_uppercase, ascii_lowercase, ascii_letters #ascii_upperc
 from itertools import product, chain, pairwise #itertools.product(range(a),range(b),[1,2,3]) allows for nested for loops in one line essentially
 from more_itertools import chunked, windowed #chunked, windowed
 from scanf import scanf
-import networkx #Networkx for graph problems like connected components
+import networkx as nx #Networkx for graph problems like connected components
 from copy import deepcopy #deepcopy() let's use copy a list without reference areas
 import portion # Interval stuff
 from z3 import Solver, Int, And
@@ -17,36 +17,36 @@ from shapely.geometry import Polygon, Point # Geometry is FUN
 #Python has complex numbers of the form x+yj (1+1j) or complex(x,y) (complex(1,1))
 
 def runPart1(filename):    
-    #lines = open(filename)
-    #lines = open(filename).read().split('\n\n')
     lines = open(filename).read().splitlines()
-    #lines = list(map(int,lines))
-    #grid = {(x,y):lines[y][x] for y in range(len(lines)) for x in range(len(lines[0]))}
-        
-    ans = None
-    for line in lines:
-        #a,b = line.split()
-        pass
-    return ans
-
+    grid = {(x,y):lines[y][x] for y in range(len(lines)) for x in range(len(lines[0]))}
+    graph = []
+    for x,y in grid:
+        for st, ed, ed_coords in [("S|JL","S|7F",(x,y-1)),("S|7F","S|JL",(x,y+1)),("S-J7","S-LF",(x-1,y)),("S-LF","S-J7",(x+1,y))]:
+            if grid[(x,y)] in st and ed_coords in grid and grid[ed_coords] in ed:
+                graph.append(((x,y), ed_coords))
+    G = nx.DiGraph()
+    G.add_edges_from(graph)
+    max_loop = sorted(list(nx.simple_cycles(G)), key=lambda x: len(x))[-1]
+    return int(len(max_loop)/2)
+    
 def runPart2(filename):    
-    #lines = open(filename)
-    #lines = open(filename).read().split('\n\n')
     lines = open(filename).read().splitlines()
-    #lines = list(map(int,lines))
-    #grid = {(x,y):lines[y][x] for y in range(len(lines)) for x in range(len(lines[0]))}
-        
-    ans = None
-    for line in lines:
-        #a,b = line.split()
-        pass
-    return ans
-        
+    grid = {(x,y):lines[y][x] for y in range(len(lines)) for x in range(len(lines[0]))}
+    graph = []
+    for x,y in grid:
+        for st, ed, ed_coords in [("S|JL","S|7F",(x,y-1)),("S|7F","S|JL",(x,y+1)),("S-J7","S-LF",(x-1,y)),("S-LF","S-J7",(x+1,y))]:
+            if grid[(x,y)] in st and ed_coords in grid and grid[ed_coords] in ed:
+                graph.append(((x,y), ed_coords))
+    G = nx.DiGraph()
+    G.add_edges_from(graph)
+    max_loop = sorted(list(nx.simple_cycles(G)), key=lambda x: len(x))[-1]
+    poly = Polygon(max_loop)
+    return sum(Point(x,y).within(poly) for x,y in grid if (x,y) not in max_loop)
+
 
 print("Running example.txt...")
 print("Part 1:", runPart1("example.txt"))
-print("Part 2:", runPart2("example.txt"))
-#print("Part 2:", runPart2("example2.txt"))
+print("Part 2:", runPart2("example2.txt"))
 print()
 print("Running input.txt...")
 print("Part 1:", runPart1("input.txt"))
