d0708bbe39633ae3e7c450097bdab65ba04ff8c5
aribchowdhury <aribchowdhury03@gmail.com>
2023-12-14 01:21:00 -0500

    Day14, 817/432 Slow Typing Speed Today, updated template with line to read graphs to np

--- day14/day14.py
+++ day14/day14.py
@@ -17,39 +17,48 @@ from shapely.geometry import Polygon, Point # Geometry is FUN, Double Resolution
 #Python has complex numbers of the form x+yj (1+1j) or complex(x,y) (complex(1,1))
 # DYNAMIC PROGRAMMING WEEEEEEEEEEEEEEE
 
-def runPart1(filename):    
-    #lines = open(filename)
-    #lines = open(filename).read().split('\n\n')
-    lines = open(filename).read().splitlines()
-    #lines = list(map(int,lines))
-    #grid = {(x,y):lines[y][x] for y in range(len(lines)) for x in range(len(lines[0]))}
-    #lines = np.array([*map(list, open(filename).read().splitlines())])
-        
-    ans = None
-    for line in lines:
-        #a,b = line.split()
-        pass
-    return ans
+def runPart1(filename): 
+    grid = np.array([*map(list, open(filename).read().splitlines())])
+    grid = np.pad(grid, 1, 'constant', constant_values='#')
+    num_rocks = 0
+    for col,row in product(range(grid.shape[1]), range(grid.shape[0]-1,-1,-1)):
+        if grid[row,col] == "O":
+            grid[row,col] = "."
+            num_rocks += 1
+        elif grid[row,col] == "#":
+            grid[row+1:row+1+num_rocks,col] = "O"
+            num_rocks = 0
+    return sum(grid.shape[0]-row-1 for col,row in product(range(grid.shape[1]),range(grid.shape[0])) if grid[row,col]=="O")
 
 def runPart2(filename):    
-    #lines = open(filename)
-    #lines = open(filename).read().split('\n\n')
-    lines = open(filename).read().splitlines()
-    #lines = list(map(int,lines))
-    #grid = {(x,y):lines[y][x] for y in range(len(lines)) for x in range(len(lines[0]))}
-    #lines = np.array([*map(list, open(filename).read().splitlines())])
-        
-    ans = None
-    for line in lines:
-        #a,b = line.split()
-        pass
-    return ans
-        
+    grid = np.array([*map(list, open(filename).read().splitlines())])
+    grid = np.pad(grid, 1, 'constant', constant_values='#')
+    def tilt():
+        num_rocks = 0
+        for col,row in product(range(grid.shape[1]), range(grid.shape[0]-1,-1,-1)):
+            if grid[row,col] == "O":
+                grid[row,col] = "."
+                num_rocks += 1
+            elif grid[row,col] == "#":
+                grid[row+1:row+1+num_rocks,col] = "O"
+                num_rocks = 0
+    seen, end_cycle = {}, None
+    for cycle in range(1000000000):
+        for _ in range(4):
+            tilt()
+            grid = np.rot90(grid, k=-1)
+        if cycle == end_cycle:
+            return sum(grid.shape[0]-row-1 for col,row in product(range(grid.shape[1]),range(grid.shape[0])) if grid[row,col]=="O")
+        if not end_cycle:
+            hash = grid.tobytes()
+            if hash in seen:
+                end_cycle = cycle+(100000000-seen[hash])%(cycle-seen[hash])+3
+            else:
+                seen[hash] = cycle
 
 print("Running example.txt...")
 print("Part 1:", runPart1("example.txt"))
 print("Part 2:", runPart2("example.txt"))
-#print("Part 2:", runPart2("example2.txt"))
 print()
 print("Running input.txt...")
 print("Part 1:", runPart1("input.txt"))
